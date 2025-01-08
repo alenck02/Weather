7e1b49aa166a5b32b1c1eafb739159681becb41c
alenck <wjdgkdusjeong@naver.com>
2025-01-08 21:32:17 +0900

    commit

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -20,6 +22,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -28,7 +32,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.weather.ui.theme.SkyBlue
 import com.example.weather.ui.theme.White
-import kotlin.math.max
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,6 +48,14 @@ class MainActivity : ComponentActivity() {
 fun WeatherTheme() {
     MaterialTheme {
         val customFont = FontFamily(Font(R.font.poppins))
+        val configuration = LocalConfiguration.current
+        val screenHeight = configuration.screenHeightDp.dp
+
+        val spacing = when {
+            screenHeight < 600.dp -> 8.dp
+            screenHeight < 900.dp -> 16.dp
+            else -> 24.dp
+        }
 
         Column(
             modifier = Modifier
@@ -54,9 +65,18 @@ fun WeatherTheme() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             WeatherHeader(customFont)
+            Spacer(modifier = Modifier.height(spacing))
+
             WeatherTemperature(customFont)
+            Spacer(modifier = Modifier.height(spacing))
+
             WeatherCondition(customFont)
+            Spacer(modifier = Modifier.height(spacing))
+
             WeatherInfoSection()
+            Spacer(modifier = Modifier.height(spacing))
+
+            WeekWeatherInfo()
         }
     }
 }
@@ -84,9 +104,9 @@ fun WeatherHeader(customFont: FontFamily) {
 @Composable
 fun WeatherTemperature(customFont: FontFamily) {
     Text(
-        text = "-2",
+        text = "-2°",
         fontFamily = customFont,
-        fontSize = 40.sp,
+        fontSize = 50.sp,
         color = White
     )
     Text(
@@ -104,13 +124,13 @@ fun WeatherCondition(customFont: FontFamily) {
     ) {
         Text(
             modifier = Modifier.padding(end = 5.dp),
-            text = "6/-5",
+            text = "6°/-5°",
             fontFamily = customFont,
             fontSize = 20.sp,
             color = White
         )
         Text(
-            text = "체감온도 -4",
+            text = "체감온도 -4°",
             fontFamily = customFont,
             fontSize = 20.sp,
             color = White
@@ -124,13 +144,86 @@ fun WeatherInfoSection() {
         modifier = Modifier
             .padding(16.dp)
             .border(BorderStroke(2.dp, Color.White))
-            .padding(16.dp)
             .fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceAround
     ) {
         WeatherInfoItem(label = "미세먼지", value = "50 µg/m³")
         WeatherInfoItem(label = "습도", value = "40%")
         WeatherInfoItem(label = "바람", value = "5 m/s")
+    }
+}
+
+@Composable
+fun WeekWeatherInfo() {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .border(BorderStroke(2.dp, Color.White))
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.SpaceAround
+    ) {
+        WeekWeatherInfoItem(
+            label = "목요일",
+            value = "20%",
+            painterResource(R.drawable.cloud),
+            painterResource(R.drawable.sunny),
+            value1 = "6",
+            value2 = "-5"
+            )
+
+        WeekWeatherInfoItem(
+            label = "금요일",
+            value = "40%",
+            painterResource(R.drawable.snowing),
+            painterResource(R.drawable.wind),
+            value1 = "2",
+            value2 = "-7"
+        )
+
+        WeekWeatherInfoItem(
+            label = "토요일",
+            value = "90%",
+            painterResource(R.drawable.umbrella),
+            painterResource(R.drawable.thunderstorm),
+            value1 = "4",
+            value2 = "1"
+        )
+
+        WeekWeatherInfoItem(
+            label = "일요일",
+            value = "20%",
+            painterResource(R.drawable.sunny),
+            painterResource(R.drawable.cloud),
+            value1 = "8",
+            value2 = "3"
+        )
+
+        WeekWeatherInfoItem(
+            label = "월요일",
+            value = "60%",
+            painterResource(R.drawable.cloud),
+            painterResource(R.drawable.snowing),
+            value1 = "1",
+            value2 = "-10"
+        )
+
+        WeekWeatherInfoItem(
+            label = "화요일",
+            value = "80%",
+            painterResource(R.drawable.umbrella),
+            painterResource(R.drawable.cloud),
+            value1 = "3",
+            value2 = "-3"
+        )
+
+        WeekWeatherInfoItem(
+            label = "수요일",
+            value = "90%",
+            painterResource(R.drawable.thunderstorm),
+            painterResource(R.drawable.umbrella),
+            value1 = "2",
+            value2 = "-6"
+        )
     }
 }
 
@@ -149,6 +242,52 @@ fun WeatherInfoItem(label: String, value: String) {
         Text(
             text = value,
             fontSize = 14.sp,
+            color = White
+        )
+    }
+}
+
+@Composable
+fun WeekWeatherInfoItem(
+    label: String,
+    value: String,
+    image1: Painter,
+    image2: Painter,
+    value1: String,
+    value2: String
+) {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 10.dp, vertical = 8.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        Text(
+            text = label,
+            fontSize = 16.sp,
+            color = White
+        )
+        Text(
+            text = value,
+            fontSize = 16.sp,
+            color = White
+        )
+        Image(
+            painter = image1,
+            contentDescription = null
+        )
+        Image(
+            painter = image2,
+            contentDescription = null
+        )
+        Text(
+            text = "$value1°",
+            fontSize = 16.sp,
+            color = White
+        )
+        Text(
+            text = "$value1°",
+            fontSize = 16.sp,
             color = White
         )
     }
